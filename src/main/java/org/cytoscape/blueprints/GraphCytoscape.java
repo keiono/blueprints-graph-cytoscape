4eb6139fa78427fb554bda886e228b4c1f41efb4
Keiichiro Ono <keiono@gmail.com>
2011-04-07 00:16:07 -0700

    Basic setting files.

---
+++ src/main/java/org/cytoscape/blueprints/GraphCytoscape.java
@@ -0,0 +1,14 @@
+package org.cytoscape.blueprints;
+
+import com.tinkerpop.blueprints.pgm.Graph;
+import com.tinkerpop.blueprints.pgm.oupls.GraphSource;
+
+public class GraphCytoscape implements GraphSource {
+
+
+	public Graph getGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
