1553f1cd76956a9df404b40f6d0da40cbb022ab2
Keiichiro Ono <keiono@gmail.com>
2011-08-05 17:58:40 -0700

    Unnecessary test case removed.

--- src/test/java/org/cytoscape/blueprints/GraphCytoscapeTest.java
+++ src/test/java/org/cytoscape/blueprints/GraphCytoscapeTest.java
@@ -1,24 +1,16 @@
 package org.cytoscape.blueprints;
 
 
-import static org.junit.Assert.*;
-
-import java.util.Collections;
+import static org.junit.Assert.assertNotNull;
 
 import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.model.AbstractCyNetworkTest;
-import org.cytoscape.model.CyEdge;
-import org.cytoscape.model.CyNode;
-import org.cytoscape.model.DummyCyNode;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.tinkerpop.blueprints.pgm.Graph;
-import com.tinkerpop.blueprints.pgm.Vertex;
-import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraphFactory;
 
 public class GraphCytoscapeTest extends AbstractCyNetworkTest{
@@ -40,26 +32,4 @@ public class GraphCytoscapeTest extends AbstractCyNetworkTest{
 	public void testGraphCytoscape() throws Exception {
 		assertNotNull(net);
 	}
-	
-	
-	@Test
-	public void testAddNodeThing() throws Exception {	
-		
-		CyNode n1 = net.addNode();
-		CyNode n2 = net.addNode();
-		CyNode n3 = net.addNode();
-		
-		assertEquals(3, net.getNodeCount());
-
-		// add a directed edge
-		net.addEdge(n1, n1, false);
-		
-		assertEquals(1, net.getEdgeCount());
-		
-		//System.out.println(net.getNeighborList(n1, CyEdge.Type.ANY).size());
-		for (CyNode c:net.getNeighborList(n1, CyEdge.Type.ANY)) {
-			//System.out.println(c.getIndex());
-		}
-	}
-
 }
