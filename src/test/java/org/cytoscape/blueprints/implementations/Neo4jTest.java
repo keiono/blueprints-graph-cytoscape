f38c7e0716f2e2fe430223e9a39bf0141d20cbf9
Keiichiro Ono <keiono@gmail.com>
2011-08-05 19:08:17 -0700

    New tests had been added for Neo4j.

--- src/test/java/org/cytoscape/blueprints/implementations/Neo4jTest.java
+++ src/test/java/org/cytoscape/blueprints/implementations/Neo4jTest.java
@@ -9,15 +9,20 @@ import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.model.AbstractCyNetworkTest;
 import org.cytoscape.model.CyNetwork;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.tinkerpop.blueprints.pgm.Edge;
 import com.tinkerpop.blueprints.pgm.Graph;
+import com.tinkerpop.blueprints.pgm.TransactionalGraph;
 import com.tinkerpop.blueprints.pgm.Vertex;
 import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
+import com.tinkerpop.blueprints.pgm.util.TransactionalGraphHelper;
+import com.tinkerpop.blueprints.pgm.util.TransactionalGraphHelper.CommitManager;
 
 /**
  * Test CyNetwork based on Neo4j implementation
@@ -26,44 +31,76 @@ import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
 public class Neo4jTest extends AbstractCyNetworkTest {
 
 	private static final String TEMP_DB = "target" + File.separator + "neo4jDB";
-	
+
 	@Mock
 	private CyEventHelper eventHelper;
-	
-	private Graph graphImplementation;	
-	
-	
-	@Before
-	public void setup() {
-		
-		MockitoAnnotations.initMocks(this);
-		
+
+	// Singleton. Actual Neo4j DB.
+	private static Graph graphImplementation;
+
+	@BeforeClass
+	public static void initialize() {
 		graphImplementation = new Neo4jGraph(TEMP_DB);
-		net = new GraphCytoscape(this.graphImplementation, eventHelper);
 	}
-	
-	@After
-	public void shutdown() throws Exception {
+
+	@AfterClass
+	public static void shutdown() throws Exception {
 		graphImplementation.shutdown();
 	}
-	
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		net = new GraphCytoscape(graphImplementation, eventHelper);
+	}
+
+	@After
+	public void clearDB() throws Exception {
+		// Remove all nodes and edges.
+		((Neo4jGraph) graphImplementation).clear();
+	}
+
 	@Test
 	public void testNeo4j() {
 		assertNotNull(this.net);
-		
+
 	}
-	
+
 	@Test
 	public void testExistingDB() {
-		//Creates very simple DB before calling CyNetwork methods
+		// Creates very simple DB before calling CyNetwork methods
 		final Vertex a = graphImplementation.addVertex(null);
 		final Vertex b = graphImplementation.addVertex(null);
 		a.setProperty("name", "node A");
 		b.setProperty("name", "node B");
 		final Edge e = graphImplementation.addEdge(null, a, b, "interacts_with");
 		e.setProperty("type", "pp");
-		
+
 		assertEquals(2, net.getNodeCount());
+		assertEquals(1, net.getEdgeCount());
+
+	}
+
+	@Test
+	public void testPerformance() {
+
+		// TODO: we need utility class to handle transactions.
+		long start = System.currentTimeMillis();
+
+		final CommitManager manager = TransactionalGraphHelper.createCommitManager(
+				(TransactionalGraph) graphImplementation, 1000);
+		for (int i = 0; i < 1000; i++) {
+			net.addNode();
+			manager.incrCounter();
+		}
+		manager.close();
+		final long time = (System.currentTimeMillis() - start);
+		System.out.println("Nodes added in " + time + " msec.");
+		
+		assertEquals(1000, net.getNodeCount());
+		
+		// If we does not use transaction utility method, this can be minutes or hours...
+		assertTrue(2000>time);
 	}
 
 }
