e9c4045d412f2a8b7da03e8bb5425c50a048e4d6
Keiichiro Ono <keiono@gmail.com>
2011-08-08 18:08:49 -0700

    Utility package added.

---
+++ src/main/java/org/cytoscape/blueprints/util/GraphConverter.java
@@ -0,0 +1,23 @@
+package org.cytoscape.blueprints.util;
+
+import org.cytoscape.model.CyNetwork;
+
+import com.tinkerpop.blueprints.pgm.Graph;
+
+public final class GraphConverter {
+	
+	/**
+	 * Convert any Blueprints graph to on-memory CyNetwork object.
+	 * @param graph
+	 * @return
+	 */
+	public static CyNetwork toCyNetwork(final Graph graph) {
+		return null;
+	}
+	
+	public static Graph toGraph(CyNetwork network) {
+		// TODO: Define factory interface for Blueprints graph.
+		
+		return null;
+	}
+}
