02fb049bd8e6c22b2e245e18398ce5889ea246a8
mckgamer <mckgamer@yahoo.com>
2011-08-28 14:01:35 -0700

    Added to GraphConverter.

--- src/main/java/org/cytoscape/blueprints/util/GraphConverter.java
+++ src/main/java/org/cytoscape/blueprints/util/GraphConverter.java
@@ -1,9 +1,13 @@
 package org.cytoscape.blueprints.util;
 
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNode;
 
 import com.tinkerpop.blueprints.pgm.Graph;
+import com.tinkerpop.blueprints.pgm.Vertex;
+import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraph;
 
 public final class GraphConverter {
 	
@@ -18,12 +22,25 @@ public final class GraphConverter {
 	 * @return
 	 */
 	public CyNetwork toCyNetwork(final Graph graph) {
+		//CyNetwork cyNet = new ArrayGraph();
 		return null;
 	}
 	
 	public Graph toGraph(CyNetwork network) {
 		// TODO: Define factory interface for Blueprints graph.
-		network.getNodeList();
+		Graph graph = new TinkerGraph();
+		for (CyNode v : network.getNodeList()) {
+			graph.addVertex(v.getSUID());
+		}
+		for (CyEdge e : network.getEdgeList()) {
+			Vertex s = graph.getVertex(e.getSource().getSUID());
+			Vertex t = graph.getVertex(e.getTarget().getSUID());
+			if (e.isDirected()) {
+				graph.addEdge(e.getSUID(), s, t, "DIRECTED");
+			} else {
+				graph.addEdge(e.getSUID(), s, t, "UNDIRECTED");
+			}
+		}
 		return null;
 	}
 }
